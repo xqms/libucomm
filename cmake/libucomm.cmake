86beefe68bccf6f0a9d98e26a35c46d027efe888
Max Schwarz <Max@x-quadraht.de>
2012-07-04 13:08:00 +0200

    libucomm.cmake: removed debug message

--- cmake/libucomm.cmake
+++ cmake/libucomm.cmake
@@ -2,7 +2,6 @@
 macro(libucomm_wrap_msg outfile msg)
 	get_filename_component(${outfile} ${msg} NAME_WE)
 	set(${outfile} ${CMAKE_CURRENT_BINARY_DIR}/${${outfile}}.h)
-	message(STATUS ${CMAKE_CURRENT_SOURCE_DIR}/${msg} -> ${${outfile}})
 	add_custom_command(
 		OUTPUT ${${outfile}}
 		DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/${msg}
