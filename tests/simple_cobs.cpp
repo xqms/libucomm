0e24f7a0d2c4555d26c7473b875853dd2c51aa0b
Max Schwarz <max.schwarz@online.de>
2015-08-21 23:50:31 +0200

    tests: use fletcher-16 for COBS test and check that it detects corruption

--- tests/simple_cobs.cpp
+++ tests/simple_cobs.cpp
@@ -13,12 +13,14 @@
 
 #include <stdio.h>
 
-typedef uc::COBSWriter<uc::InvertedModSumGenerator, BufferIO> EnvelopeWriter;
+typedef uc::Fletcher16Generator ChecksumGenerator;
+
+typedef uc::COBSWriter<ChecksumGenerator, BufferIO> EnvelopeWriter;
 
 typedef uc::IO<EnvelopeWriter, uc::IO_W> SimpleWriter;
 typedef Proto<SimpleWriter> WProto;
 
-typedef uc::COBSReader<uc::InvertedModSumGenerator, 1024> EnvelopeReader;
+typedef uc::COBSReader<ChecksumGenerator, 1024> EnvelopeReader;
 typedef uc::IO<EnvelopeReader, uc::IO_R> SimpleReader;
 typedef Proto<SimpleReader> RProto;
 
@@ -46,29 +48,92 @@ TEST_CASE("simple_cobs", "[cobs]")
 	EnvelopeReader input;
 	while(dbg.isCharAvailable())
 	{
-		if(input.take(dbg.getChar()) == EnvelopeReader::NEW_MESSAGE)
+		EnvelopeReader::TakeResult ret = input.take(dbg.getChar());
+
+		switch(ret)
 		{
-			REQUIRE(input.msgCode() == RProto::Message::MSG_CODE);
-
-			RProto::Message pkt2;
-			input >> pkt2;
-
-			REQUIRE(pkt2.flags == pkt.flags);
-
-			RProto::Struct data;
-			int i = 0;
-			while(pkt2.list.next(&data))
+			case EnvelopeReader::NEW_MESSAGE:
 			{
-				REQUIRE(data.index == i);
-				REQUIRE(data.some_value == 5*i);
-				++i;
+				REQUIRE(input.msgCode() == RProto::Message::MSG_CODE);
+
+				RProto::Message pkt2;
+				input >> pkt2;
+
+				REQUIRE(pkt2.flags == pkt.flags);
+
+				RProto::Struct data;
+				int i = 0;
+				while(pkt2.list.next(&data))
+				{
+					REQUIRE(data.index == i);
+					REQUIRE(data.some_value == 5*i);
+					++i;
+				}
+
+				REQUIRE(i == 4);
+
+				packetCount++;
+				break;
 			}
-
-			REQUIRE(i == 4);
-
-			packetCount++;
+			case EnvelopeReader::NEED_MORE_DATA:
+				break;
+			case EnvelopeReader::CHECKSUM_ERROR:
+				FAIL("Checksum error");
+				break;
+			case EnvelopeReader::FRAME_ERROR:
+				FAIL("Frame error");
+				break;
+			default:
+				FAIL("Unknown error");
+				break;
 		}
 	}
 
 	REQUIRE(packetCount == 2);
+}
+
+TEST_CASE("corrupt_cobs", "[cobs]")
+{
+	WProto::Message pkt;
+	pkt.flags = 0;
+	pkt.list.setCallback(fillStruct, 4);
+
+	BufferIO dbg;
+	EnvelopeWriter output(&dbg);
+	output << pkt;
+
+	int checksumErrors = 0;
+
+	EnvelopeReader input;
+	int count = 0;
+
+	while(dbg.isCharAvailable())
+	{
+		uint8_t byte = dbg.getChar();
+
+		if(++count == 5)
+			byte |= (1 << 3);
+
+		EnvelopeReader::TakeResult ret = input.take(byte);
+
+		switch(ret)
+		{
+			case EnvelopeReader::NEW_MESSAGE:
+				FAIL("Got message even though it was corrupted");
+				break;
+			case EnvelopeReader::NEED_MORE_DATA:
+				break;
+			case EnvelopeReader::CHECKSUM_ERROR:
+				checksumErrors++;
+				break;
+			case EnvelopeReader::FRAME_ERROR:
+				FAIL("Frame error");
+				break;
+			default:
+				FAIL("Unknown error");
+				break;
+		}
+	}
+
+	REQUIRE(checksumErrors == 1);
 }
