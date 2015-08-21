f8b5488a015b374116a45189fe7fd548ffc32fb3
Max Schwarz <max.schwarz@online.de>
2015-08-21 23:49:47 +0200

    checksum.h: fletcher-16 implementation

--- include/libucomm/checksum.h
+++ include/libucomm/checksum.h
@@ -4,6 +4,8 @@
 #ifndef CHECKSUM_H
 #define CHECKSUM_H
 
+#include <stdint.h>
+
 namespace uc
 {
 
@@ -53,6 +55,34 @@ private:
 	uint8_t m_value;
 };
 
+/**
+ * @brief Fletcher-16 checksum generator
+ **/
+class Fletcher16Generator
+{
+public:
+	typedef uint16_t SumType;
+
+	void reset()
+	{
+		m_sum1 = 0;
+		m_sum2 = 0;
+	}
+
+	void add(uint8_t c)
+	{
+		m_sum1 = (m_sum1 + c) % 255;
+		m_sum2 = (m_sum2 + m_sum1) % 255;
+	}
+
+	uint16_t value() const
+	{
+		return (m_sum2 << 8) | m_sum1;
+	}
+private:
+	uint16_t m_sum1;
+	uint16_t m_sum2;
+};
 
 }
 
